11ccb3cb757c7191f51e1694191c258ec301a9b6
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-11-04 00:55:45 +0100

    adding main file

---
+++ prompt.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int ac, char *av[])
+{
+	(void)ac, (void)av;
+	char *line = NULL;
+	size_t len;
+
+	do 
+	{
+		printf("my_bash$ ");
+		if (getline(&line, &len, stdin) == -1)
+		{
+			if (feof(stdin))
+			{
+				printf("\n");
+				printf("EXIT_SUCCESS");
+				printf("\n");
+				exit(EXIT_SUCCESS);
+			}
+			else
+			{
+				perror("readline");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+	while (1);
+
+	free(line);
+	return (0);
+}
